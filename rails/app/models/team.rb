2317c8f321130eb5eb4580663d2050a9996ad958
Ben Prew <ben.prew@gmail.com>
2008-05-18 02:00:45 +0000

    add reminder email functionality to team

--- rails/app/models/team.rb
+++ rails/app/models/team.rb
@@ -1,8 +1,11 @@
+require 'date'
+
 class Team < ActiveRecord::Base
   belongs_to :division
   has_many :games
   has_and_belongs_to_many :players
 
   def next_unreminded_game
+    games.grep { |g| g.date >= Date.today() }.grep { |g| !g.reminder.sent }[0]
   end
 end
