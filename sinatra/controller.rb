b171d972bcbbf751cea90a6f65dda86a928aff09
Ben Prew <ben.prew@gmail.com>
2008-06-20 06:12:54 +0000

    first checkin of new sinatra web framework

---
+++ sinatra/controller.rb
@@ -0,0 +1,35 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'sinatra'
+require 'user'
+
+get '/' do
+  erb :index
+end
+
+post '/search' do
+  teams = Team.find(:all,
+                    :conditions => [ "name like ?", '%' + params[:team].upcase + '%' ],
+                    :order => "name")
+
+  if teams.length == 0
+    print "<h1> No teams found </h1>"
+  elsif teams.length == 1
+    redirect('/team?team_id='+teams[0].id.to_s)
+  else
+    erb :search
+  end
+end
+
+get '/browse' do
+  divisions = Division.find(:all,
+                            :conditions => [ 'league = ?', params[:league].sort { |a, b| a.name <=> b.name }
+  haml :browse
+end
+
+get '/team' do
+  team = Team.find(params[:team_id])
+
+  haml :team_home
+end
