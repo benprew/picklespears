23acadfd5f8c04d372e87afb7534e9b911029638
Ben Prew <ben.prew@gmail.com>
2009-08-21 00:26:02 -0600

    update for 1st fall, fix a bug where a team with the same name can be in different divisions

--- bin/update_schedule_from_files.rb
+++ bin/update_schedule_from_files.rb
@@ -25,25 +25,33 @@ Dir.glob('*.txt').each do |filename|
     team = items.shift
     game_date = items.shift
     game_description = items.join " "
-    t = Team.first(:name => team)
-    if !t
-      warn "no team found for #{team} : #{filename}"
-      next
-    end
     league = \
       filename[0,1] == 'm' ? 'Men' \
       : filename[0,1] == 'w' ? 'Women' \
       : 'Coed'
     possible_new_div = Division.first_or_create(:name => filename.match(/[^.]+/)[0], :league => league)
-    if !possible_new_div || t.division.name != possible_new_div.name
-      warn "TEAM: #{team} - Updating division from #{t.division.name} to #{possible_new_div.name}"
-      t.division = possible_new_div
-      t.save
-      possible_new_div.save
+    teams = Team.all(:name => team)
+    if teams.length == 0
+      warn "no team found for '#{team}' : #{filename}"
+      next
     end
-    next if Game.first(:description => game_description, :team_id => t.id )
-    warn "building games for #{team}"
-    g = Game.new(:date => game_date, :description => game_description, :team => t)
-    g.save
+    found_team = false
+    teams.each do |t|
+      
+      next unless t.division.league == possible_new_div.league
+
+      found_team = true
+      if possible_new_div && t.division.name != possible_new_div.name
+        warn "TEAM: #{team} - Updating division from #{t.division.name} to #{possible_new_div.name}"
+        t.division = possible_new_div
+        t.save
+        possible_new_div.save
+      end
+      next if Game.first(:description => game_description, :team_id => t.id )
+      warn "building games for #{team}"
+      g = Game.new(:date => game_date, :description => game_description, :team => t)
+      g.save
+    end
+    warn "no team found for '#{team}' : #{filename}" unless found_team
   end
 end
