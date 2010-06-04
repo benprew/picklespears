f207b1a63fa2afa8232278db50e8caee5f47f117
Ben Prew <ben.prew@gmail.com>
2010-06-04 00:24:04 -0700

    update db class to handle sqlite3 files correctly

--- lib/picklespears/db.rb
+++ lib/picklespears/db.rb
@@ -4,13 +4,17 @@ require 'yaml'
 
 def make_connect_string(db_config, environment)
   db_info = db_config[environment.to_s]
-  return sprintf "%s://%s:%s@localhost/%s", db_info['adapter'], db_info['username'], db_info['password'], db_info['database']
+  if db_info['adapter'] == 'sqlite3'
+    return sprintf "%s:///var/db/%s", db_info['adapter'], db_info['database']
+  else
+    return sprintf "%s://%s:%s@localhost/%s", db_info['adapter'], db_info['username'], db_info['password'], db_info['database']
+  end
 end
 
 db_config = YAML::load File.open(File.dirname(__FILE__) + '/../../config/database.yml')
 
 if test?
-  DataMapper.setup(:default, 'sqlite3:///tmp/test_db')
+  DataMapper.setup(:default, make_connect_string(db_config, :test))
   DataMapper.auto_migrate!
 elsif production?
   DataMapper.setup(:default, make_connect_string(db_config, :production))
