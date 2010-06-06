2f381196b1eef82b1d10b5439ed86c8718217e40
Ben Prew <ben.prew@gmail.com>
2010-06-05 22:24:14 -0700

    modify config and db library for use with Heroku

--- lib/picklespears/db.rb
+++ lib/picklespears/db.rb
@@ -3,13 +3,15 @@ require 'dm-core'
 require 'yaml'
 
 def make_connect_string(db_config, environment)
+  # for Heroku
+  return ENV['DATABASE_URL'] if ENV['DATABASE_URL']
+
   db_info = db_config[environment.to_s]
+
   if db_info['adapter'] == 'sqlite3'
     return sprintf "%s:///var/db/%s", db_info['adapter'], db_info['database']
   else
-    db_str = sprintf "%s://%s:%s@localhost/%s", db_info['adapter'], db_info['username'], db_info['password'], db_info['database']
-    warn db_str
-    return db_str
+    return sprintf "%s://%s:%s@localhost/%s", db_info['adapter'], db_info['username'], db_info['password'], db_info['database']
   end
 end
 
