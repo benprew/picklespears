b7a0c4243ce9ad1b2f71c2318e429d38a9f0e85f
Ben Prew <ben.prew@gmail.com>
2010-12-09 21:06:39 -0800

    update schedule games to use round-based algorithm instead of trying to schedule games individually

--- bin/schedule_games.rb
+++ bin/schedule_games.rb
@@ -2,83 +2,35 @@
 
 require 'date'
 
-def build_games(teams)
-  team_games = {}
-  teams.sort.each do |team|
-    team_games[team] ||= []
-    teams.sort.each do |other_team|
-      team_games[other_team] ||= []
-      break if team_games[team].length == 8
-      next if team == other_team
-      game = ((rand * 2).to_i == 0 ? [team, other_team] : [other_team, team])
-      team_games[team].push(game)
-    end
-
-#     while(team_games[team].length < 8) do
-#       teams.shuffle.each do |other_team|
-#         next if team_games[other_team].length == 8
-#         break if team_games[team].length == 8
-#         game = ((rand * 2).to_i == 0 ? [team, other_team] : [other_team, team])
-#         team_games[team].push(game)
-#         team_games[other_team].push(game)
-#       end
-#    end
+class Array
+  def slide_down
+    return self[1, self.length] + [self[0]]
   end
-  p team_games
-  team_games.each do |key, val| 
-    puts key + "    " + val.length.to_s
-    p length
-  end
-
-
-  team_games
 end
 
-def schedule_games(date, games)
+def build_games(teams)
+  rounds = []
+  rounds_needed = teams.length % 2 == 0 ? 8 : 8 + ((8 - teams.length) / 2.0).ceil
+
+  while rounds.length < rounds_needed do
+    mid = teams.length / 2
+    first_half = teams[0, mid]
+    last_half = teams[mid, teams.length]
+    rounds += [ first_half.zip last_half.reverse ]
+    teams = teams.slide_down
+  end
+  rounds
+end
+
+def schedule_game(date, game)
   # if before Friday, advance until Friday
   while (date.wday < 4 ) do
     date += 1
   end
 
-  play_times = 17..24.map { |x| [ 4, x ] }
-  play_times += 10..24.map { |x| [ 5, x ] }
-  play_times += 9..22.map { |x| [ 6, x ] }
-
-  p play_times
+  play_times = (17..24).map { |x| [ 4, x ] } + (10..24).map { |x| [ 5, x ] } + (9..22).map { |x| [ 6, x ] }
   
 end
-
-def has_more_games?()
-  @teams_games.map { |team, games| games.legth }.max > 0
-end
-
-
-
-def build_games_for_team(team, other_teams)
-  randomized = other_teams.shuffle
-  team_games = {}
-  team_games[team] = []
-  
-  while(team_games[team].length != 8) do
-    if randomized.length == 0 then
-      randomized = other_teams.shuffle
-    end
-    other_team = randomized.shift
-    game = ((rand * 2).to_i == 0 ? [team, other_team] : [other_team, team])
-      
-    team_games[team].push(game)
-    team_games[other_team] = [] unless team_games[other_team]
-    team_games[other_team].push(game)
-  end
-
-  team_games.each do |key, val|
-    puts key
-    p val
-  end
-  team_games
-  
-end
-
 
 play_times = [
   [ 0, [ 17..22 ] ], #mon
@@ -105,12 +57,10 @@ teams.each do |team_info|
   division = team_info[1]
   teams = team_info[2]
 
-  @team_games = build_games(teams)
   date = Date.today()
-  while(has_more_games?(@team_games)) do
-    schedule_games(date, @team_games)
+  build_games(teams).each do |round|
+    round.each do |game|
+      schedule_game date, round
+    end
   end
-  
 end
-
-
