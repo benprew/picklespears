b3a2ed640779db756bbc3ad628ddeb064df05bf6
Ben Prew <ben.prew@gmail.com>
2011-12-16 09:23:59 -0700

    new sendgrid sending info

--- picklespears.rb
+++ picklespears.rb
@@ -140,12 +140,13 @@ class PickleSpears < Sinatra::Application
           :content_type => 'text/html',
           :via => :smtp,
           :via_options => {
-            :address        => "smtp.sendgrid.net",
-            :port           => "25",
-            :authentication => :plain,
-            :user_name      => ENV['SENDGRID_USERNAME'],
-            :password       => ENV['SENDGRID_PASSWORD'],
-            :domain         => ENV['SENDGRID_DOMAIN'],
+	    :address => 'smtp.sendgrid.net',
+	    :port => '587',
+	    :domain => 'heroku.com',
+	    :user_name => ENV['SENDGRID_USERNAME'],
+	    :password => ENV['SENDGRID_PASSWORD'],
+	    :authentication => :plain,
+	    :enable_starttls_auto => true
           }
         }
         if production?
