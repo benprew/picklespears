e8cc2fefd7c3df807f39eed18b919f0142b321f3
Ben Prew <ben.prew@gmail.com>
2012-06-13 15:29:21 -0700

    Upgrade Vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -18,7 +18,7 @@ Vagrant::Config.run do |config|
 
   # Assign this VM to a host only network IP, allowing you to access it
   # via the IP.
-  config.vm.network "33.33.33.10"
+  config.vm.network :hostonly, "33.33.33.10"
 
   config.vm.customize do |vm|
       vm.memory_size = 1024
@@ -39,57 +39,11 @@ Vagrant::Config.run do |config|
   # are contained in a directory path relative to this Vagrantfile.
   # You will need to create the manifests directory and a manifest in
   # the file base.pp in the manifests_path directory.
-  #
-  # An example Puppet manifest to provision the message of the day:
-  #
-  # # group { "puppet":
-  # #   ensure => "present",
-  # # }
-  # #
-  # # File { owner => 0, group => 0, mode => 0644 }
-  # #
-  # # file { '/etc/motd':
-  # #   content => "Welcome to your Vagrant-built virtual machine!
-  # #               Managed by Puppet.\n"
-  # # }
-  #
+
+
   config.vm.provision :puppet do |puppet|
     puppet.manifests_path = "manifests"
     puppet.manifest_file  = "base.pp"
+    puppet.module_path = "manifests/modules"
   end
-
-  # Enable provisioning with chef solo, specifying a cookbooks path (relative
-  # to this Vagrantfile), and adding some recipes and/or roles.
-  #
-  # config.vm.provision :chef_solo do |chef|
-  #   chef.cookbooks_path = "cookbooks"
-  #   chef.add_recipe "mysql"
-  #   chef.add_role "web"
-  #
-  #   # You may also specify custom JSON attributes:
-  #   chef.json = { :mysql_password => "foo" }
-  # end
-
-  # Enable provisioning with chef server, specifying the chef server URL,
-  # and the path to the validation key (relative to this Vagrantfile).
-  #
-  # The Opscode Platform uses HTTPS. Substitute your organization for
-  # ORGNAME in the URL and validation key.
-  #
-  # If you have your own Chef Server, use the appropriate URL, which may be
-  # HTTP instead of HTTPS depending on your configuration. Also change the
-  # validation key to validation.pem.
-  #
-  # config.vm.provision :chef_client do |chef|
-  #   chef.chef_server_url = "https://api.opscode.com/organizations/ORGNAME"
-  #   chef.validation_key_path = "ORGNAME-validator.pem"
-  # end
-  #
-  # If you're using the Opscode platform, your validator client is
-  # ORGNAME-validator, replacing ORGNAME with your organization name.
-  #
-  # IF you have your own Chef Server, the default validation client name is
-  # chef-validator, unless you changed the configuration.
-  #
-  #   chef.validation_client_name = "ORGNAME-validator"
 end
--- manifests/base.pp
+++ manifests/base.pp
@@ -1,35 +1,40 @@
-# import "postgres"
+import "postgres"
 
 Exec { path => "/usr/bin:/bin:/usr/sbin" }
 
-package { "libsqlite3-dev"     : ensure => installed }
+package { "curl"               : ensure => installed }
 package { "g++"                : ensure => installed }
-package { "libpq-dev"          : ensure => installed }
-package { "ruby1.9.1"          : ensure => installed }
-package { "nginx"              : ensure => installed }
 package { "git-core"           : ensure => installed }
-package { "vim"                : ensure => installed }
-package { "libshadow-ruby1.8"  : ensure => installed }
 package { "libmysqlclient-dev" : ensure => installed }
+package { "libpq-dev"          : ensure => installed }
+package { "libsqlite3-dev"     : ensure => installed }
 package { "libxml2-dev"        : ensure => installed }
 package { "libxslt1-dev"       : ensure => installed }
+package { "nginx"              : ensure => installed }
 package { "postgresql-8.4"     : ensure => installed, require => Exec["apt-update"] }
 package { "rake"               : ensure => installed }
+package { "ruby1.9.1"          : ensure => installed }
+package { "vim"                : ensure => installed }
 
-# postgres::database { "picklespears":
-#   ensure => present,
-#   name => 'picklespears',
-#   require => Package['postgresql-8.4'],
-# }
+postgres::database { "picklespears":
+  ensure => present,
+  name => 'picklespears',
+  require => Package['postgresql-8.4'],
+}
 
-# postgres::role { "picklespears":
-#   password => "*6F6FAA28F5A830A76C08AA0EDB4E4DF5B0A36C35",
-#   ensure => present,
-# }
+postgres::role { "picklespears":
+  password => "md570a9605e0eb7892dd928b47db8e2d0ca",
+  ensure => present,
+  require => Package['postgresql-8.4'],
+}
 
 exec { "apt-update":
         command     => "/usr/bin/apt-get update",
         refreshonly => true;
+}
+
+group { "puppet":
+  ensure => "present",
 }
 
 exec { "/opt/ruby/bin/gem install bundler": }
@@ -48,27 +53,4 @@ file { "/var/run":
 file { "/var/log":
   ensure => directory,
   mode   => 755,
-}
-
-user { "picklespears":
-  home     => '/home/picklespears',
-  password => '$6$y8svKzg8$xUgt6TKLMxzc4o9nstWRxQDMrsnNC48Yq/BzQqLEVeLlkBBc5MAtvJiKsAIV0SncRJsGSrFAJu39Nn5vpLPk3/',
-  shell    => '/bin/bash',
-  groups   => 'users',
-  require  => Package['libshadow-ruby1.8'],
-  ensure   => present,
-}
-
-file { "/home/picklespears":
-  ensure  => directory,
-  owner   => 'picklespears',
-  group   => 'users',
-  require => User["picklespears"],
-}
-
-file { "/var/www/picklespears":
-  ensure  => directory,
-  owner   => 'picklespears',
-  group   => 'users',
-  require => User["picklespears"],
 }
