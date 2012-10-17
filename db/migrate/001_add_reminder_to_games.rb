8cfb3ef5d5a007f05ce458866eb72295880ea4b6
Ben Prew <ben.prew@gmail.com>
2012-10-16 23:27:59 -0700

    remove old schema and migration files

--- db/migrate/001_add_reminder_to_games.rb
+++
@@ -1,9 +0,0 @@
-class AddReminderToGames < ActiveRecord::Migration
-  def self.up
-    add_column :games, :reminder_sent, :boolean, :null => false, :default => false
-  end
-
-  def self.down
-    remove_column :games, :reminder_sent
-  end
-end
--- db/migrate/002_add_password_to_player.rb
+++
@@ -1,9 +0,0 @@
-class AddPasswordToPlayer < ActiveRecord::Migration
-  def self.up
-    add_column :users, :password, :string, :limit => 32
-  end
-
-  def self.down
-    remove_column :users, :password
-  end
-end
