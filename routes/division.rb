5b2be31d6c0e0e5dd99fd149c04b22d1d945d5d1
Ben Prew <ben.prew@gmail.com>
2012-12-30 14:51:26 -0800

    division helpers

--- routes/division.rb
+++ routes/division.rb
@@ -2,4 +2,16 @@ class PickleSpears < Sinatra::Application
   get '/division/schedule_games' do
     haml :schedule_games
   end
+
+  get '/division/list' do
+    divisions = Division.order(:name).all.select { |d| d.teams_with_upcoming_games.length > 0 }
+    haml '/division/list'.to_sym, locals: { divisions: divisions }
+  end
+
+  get '/division' do
+    @divisions = Division.order(:name).all.select { |d| d.teams_with_upcoming_games.length > 0 }
+    @games = Game.where(teams: Team.where(division: Division[params[:division_id]]) ).order(:date)
+
+    haml 'division/index'.to_sym
+  end
 end
