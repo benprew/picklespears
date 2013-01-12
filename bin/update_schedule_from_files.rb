59405dd2bf9ef17ae04addb97855e739dacd98ab
Ben Prew <ben.prew@gmail.com>
2013-01-11 17:58:49 -0800

    changes for new season

--- bin/update_schedule_from_files.rb
+++ bin/update_schedule_from_files.rb
@@ -4,9 +4,6 @@ require 'pp'
 
 require_relative '../picklespears'
 
-missing_teams = Set.new
-@@force_team_create = true
-
 def deal_with_missing_team(name, division, game_date, game_description, is_home_team=false)
   if @@force_team_create == true
     warn "Creating Team: #{name}"
@@ -14,33 +11,22 @@ def deal_with_missing_team(name, division, game_date, game_description, is_home_
       :date => game_date,
       :description => game_description,
     )
-    TeamsGames.find_or_create(
+    TeamsGame.unrestrict_primary_key
+    TeamsGame.find_or_create(
       game_id: game.id,
-      team_id: Team.create(:name => name, :division_id => division.id),
+      team_id: Team.create(:name => name, :division_id => division.id).id,
       ) { |tg| tg.is_home_team = is_home_team}
+    TeamsGame.restrict_primary_key
   else
     missing_teams << "#{name} #{division.name}"
   end
 end
 
-ARGV.each do |filename|
-  puts filename
-  f = File.new(filename)
-  f.each do |line|
-    line.chop!
-    (league, division, home, away, game_date, game_description) = line.split "|"
-
-    division = Division.first(:name => division, :league => league)
-    add_game_for_team(division, home, true)
-    add_game_for_team(division, away, false)
-  end
-end
-
-def add_game_for_team(division, team, is_home_team=false)
+def add_game_for_team(division, team, is_home_team=false, game_date, game_description)
   teams = Team.filter(:name => team).all
   if teams.length == 0
     deal_with_missing_team(team, division, game_date, game_description, is_home_team)
-    next
+    return
   end
   found_team = false
 
@@ -57,13 +43,33 @@ def add_game_for_team(division, team, is_home_team=false)
       :date => game_date,
       :description => game_description
       )
+
+    TeamsGame.unrestrict_primary_key
     TeamsGame.find_or_create(
       game_id: game.id,
-      team_id: team.id
+      team_id: t.id
       ) { |tg| tg.is_home_team = is_home_team }
+    TeamsGame.restrict_primary_key
   end
 
   deal_with_missing_team(team, division, game_date, game_description, is_home_team) unless found_team
+end
+
+missing_teams = Set.new
+@@force_team_create = true
+
+ARGV.each do |filename|
+  puts filename
+  f = File.new(filename)
+  f.each do |line|
+    line.chop!
+    (league_name, division, home, away, game_date, game_description) = line.split "|"
+
+    league = League.first(:name => league_name)
+    division = Division.first(:name => division, :league => league)
+    add_game_for_team(division, home, true, game_date, game_description)
+    add_game_for_team(division, away, false, game_date, game_description)
+  end
 end
 
 if missing_teams.length > 0
