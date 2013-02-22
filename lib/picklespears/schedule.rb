9fcda81663651d79aa65e478ac8febb0941f3083
Ben Prew <ben.prew@gmail.com>
2013-02-22 08:12:14 -0800

    store games as array, not as hash and fix swap_games method

--- lib/picklespears/schedule.rb
+++ lib/picklespears/schedule.rb
@@ -5,19 +5,15 @@ class Schedule
 
   GAME_LENGTH_IN_MINUTES = 50
 
-  attr :time_slots, :current_week, :game_times, :scheduled_times
+  attr :time_slots, :current_week, :game_times, :games
 
   def initialize(season_start_date, time_slots)
     @time_slots = time_slots
     @current_week = season_start_date
     @game_times = build_for_week(@current_week)
     @max_games_for_day = Hash.new(0)
-    @scheduled_times = {}
+    @games = []
     @team_weeks = Hash.new(Set.new)
-  end
-
-  def games
-    @scheduled_times.select { |k, v| v.team_ids && v.team_ids.length > 0 }.sort_by { |arr| arr[0] }.map{ |a| game = a[1]; game.date = a[0]; game }
   end
 
   def first_game_of_day?(date)
@@ -36,10 +32,10 @@ class Schedule
     date == time + (60 * Schedule::GAME_LENGTH_IN_MINUTES * slot[:slot_info].num_games)
   end
 
-  def next_for_week(week, game)
-    raise "One or more teams are already playing in week: #{week}" if !same_week?(week, game.date) && teams_have_game_this_week(week.strftime('%W'), game.team_ids)
-    game_time = @game_times.select { |gt| same_week?(week, gt.date.to_date) && gt.league_ids.include?(game.league_id) }.first
-    game_time && game_time.date
+  def next_for_week(date, game)
+    raise "One or more teams are already playing in week: #{date}" if !same_week?(date, game.date) && teams_have_game_this_week(date, game.team_ids)
+    game_time = @game_times.index { |gt| same_week?(date, gt.date) && gt.league_ids.include?(game.league_id) }
+    return @game_times.delete_at(game_time) if game_time
   end
 
   def same_week?(week1, week2)
@@ -50,36 +46,27 @@ class Schedule
     game_time = self.next(game.team_ids, game.league_id)
     game_time.team_ids = game.team_ids
     game_time.league_id = game.league_id
-    date = game_time.delete_field(:date)
-    @team_weeks[date.strftime('%W')] += game.team_ids
-    @scheduled_times[date] = game_time
-  end
-
-  def empty_game_dates
-    @scheduled_times.select { |time, game_slot| !game_slot.team_ids }.keys
+    @team_weeks[game_time.date.strftime('%W')] += game.team_ids
+    @games << game_time
   end
 
   def next(team_pairing, league_id)
     if @game_times.length < 1
       @current_week += 7
       @game_times = build_for_week(@current_week)
-      @game_times.each { |gt| raise "Duplicate time" if @scheduled_times[gt.date]; @scheduled_times[gt.date] = gt }
     end
 
     game_time_index = nil
     while (!game_time_index) do
       @game_times.each_index do |i|
-        week_no = @game_times[i].date.strftime('%W')
-        if @game_times[i].league_ids.include?(league_id) && !teams_have_game_this_week(week_no, team_pairing)
+        if @game_times[i].league_ids.include?(league_id) && !teams_have_game_this_week(@game_times[i].date, team_pairing)
           game_time_index = i
           break
         end
       end
       if !game_time_index
         @current_week += 7
-        new_times = build_for_week(@current_week)
-        new_times.each { |gt| raise "Duplicate time: #{gt.date} keys: #{@scheduled_times.keys}" if @scheduled_times[gt.date]; @scheduled_times[gt.date] = gt }
-        @game_times += new_times
+        @game_times += build_for_week(@current_week)
       end
     end
 
@@ -99,41 +86,50 @@ class Schedule
     raise "TODO: Not implemented"
   end
 
+  def index_for_date(date)
+    @games.index { |g| g.date == date }
+  end
+
+  def game_for_date(date)
+    @games[index_for_date(date)]
+  end
+
   # swaps the game weeks, choosing the first available time in that
   # week for the game, this compresses the schedule and means that a
   # single mutation can remove a game following an empty game day,
   # instead of multiple mutations if we did a strict datetime swap
   def swap_game_weeks(game1_date)
+    game1_index = index_for_date(game1_date)
     other_game_index = nil
-    game2_date = nil
+    game2_index = nil
 
     # this is bad, we should be looking for games that will swap into
     # another week and fill an empty slot, not just swapping for empty
     # slots
-
-    # this should include available slots, as we will want to swap into an empty slot as often as possible
-    (empty_game_dates + games().shuffle.map(&:date)).each do |date|
-      if swappable?(@scheduled_times[game1_date], @scheduled_times[date])
-        game2_date = date
+    @games.shuffle.each_index do |index|
+      if swappable?(@games[game1_index], @games[index])
+        game2_index = index
         break
       end
     end
 
-    raise "Could not find a game to swap for #{@scheduled_times[game1_date]}" unless game2_date
+    raise "Could not find a game to swap for #{@games[game1_index]}" unless game2_index
 
     # schedule has a list of game_dates that it hasn't used, so we can ask it for the next game
     # swap needs to swap position AND game dates, not either/or
-    new_other_date =
-      [next_for_week(game1_date, @scheduled_times[game2_date]), game1_date].delete_if { |o| o == nil }.min
+    new_other_slot =
+      [next_for_week(@games[game1_index].date, @games[game2_index]), @games[game1_index]].delete_if { |o| o == nil }.min_by{ |t| t.date }
 
     # this is a little hinky, but should be fine after game swap
-    if new_other_date != game1_date
+    if new_other_slot.date != @games[game1_index].date
       # puts "other: #{@games[other_game_index]} new date: #{new_other_date}"
       # puts "game1: #{@games[game_index]}"
-      swap_games!(new_other_date, game1_date)
+      game = @games[game1_index]
+      @games[game1_index] = OpenStruct.new( date: new_other_slot.date, league_ids: new_other_slot.league_ids, team_ids: game.team_ids )
+      @game_times << OpenStruct.new( date: game.date, league_ids: game.league_ids )
     end
 
-    swap_games!(new_other_date, game2_date)
+    swap_games!(game1_index, game2_index)
   end
 
   private
@@ -142,8 +138,8 @@ class Schedule
     @time_slots.select{ |slot| slot[:slot_info].cwday == (date.wday + 1) }.first
   end
 
-  def teams_have_game_this_week(week_no, teams)
-    !(@team_weeks[week_no] & teams).empty?
+  def teams_have_game_this_week(date, teams)
+    !(@team_weeks[date.strftime('%W')] & teams).empty?
   end
 
   def build_for_week(week)
@@ -167,7 +163,7 @@ class Schedule
   end
 
   def swappable?(game1, game2)
-    in_scheduled_time?(game1, game2) && !teams_have_game_this_week(game2.strftime('%W'), game1.team_ids)
+    in_scheduled_time?(game1, game2) && !teams_have_game_this_week(game2.date, game1.team_ids)
   end
 
   def in_scheduled_time?(game1, game2)
@@ -187,14 +183,19 @@ class Schedule
     time + (60 * game_length_in_minutes * games_to_add)
   end
 
-  def swap_games!(game1_date, game2_date)
+  def swap_games!(game1_index, game2_index)
     # league ids is property of the date slot, not of the teams playing
-    tmp_league_ids = @scheduled_times[game1_date].league_ids
-    @scheduled_times[game1_date].league_ids = @scheduled_times[game2_date].league_ids
-    @scheduled_times[game2_date].league_ids = tmp_league_ids
+    tmp_league_ids = @games[game1_index].league_ids
+    @games[game1_index].league_ids = @games[game2_index].league_ids
+    @games[game2_index].league_ids = tmp_league_ids
 
-    tmp_game = @scheduled_times[game1_date]
-    @scheduled_times[game1_date] = @scheduled_times[game2_date]
-    @scheduled_times[game2_date] = tmp_game
+    # league ids is property of the date slot, not of the teams playing
+    tmp_date = @games[game1_index].date
+    @games[game1_index].date = @games[game2_index].date
+    @games[game2_index].date = tmp_date
+
+    tmp_game = @games[game1_index]
+    @games[game1_index] = @games[game2_index]
+    @games[game2_index] = tmp_game
   end
 end
