6d5a0c2c050b9c7d241ded18d2ad9430cdc2a06f
Ben Prew <ben.prew@gmail.com>
2013-03-14 12:43:58 -0700

    remove empty schedule url

--- picklespears.rb
+++ picklespears.rb
@@ -32,10 +32,6 @@ class PickleSpears < Sinatra::Application
     set :clean_trace, true
     require 'newrelic_rpm'
 
-    get error do
-      send_email to: 'ben.prew@gmail.com', subject: 'error on teamvite.com', body: request.env['sinatra.error'].message
-      "Application error."
-    end
   end
 
   before do
@@ -45,8 +41,9 @@ class PickleSpears < Sinatra::Application
     end
   end
 
-  get '/schedule' do
-
+  get error do
+    send_email to: 'ben.prew@gmail.com', subject: 'error on teamvite.com', body: request.env['sinatra.error'].message
+    "Application error."
   end
 
   get '/' do
