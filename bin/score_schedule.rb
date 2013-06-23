56b17890e9bb5710564dae05270fe474294f5f56
Ben Prew <ben.prew@gmail.com>
2013-06-23 01:07:07 -0700

    utility for scoring a schedule in plain text format

---
+++ bin/score_schedule.rb
@@ -0,0 +1,107 @@
+#!/usr/bin/env ruby
+
+# Expects a schedule in tab-delimited format with the following fields
+# Game Date
+# Home Team
+# Away Team
+
+require 'csv'
+
+require_relative '../picklespears'
+require 'picklespears/schedule'
+require 'picklespears/schedule_builder'
+
+def score_schedule(builder)
+  empty_game_times = 0
+  crappy_game_times = 0
+  nonpreferred_game_time = 0
+  games_on_requested_days_off = 0
+
+  builder.calc_score_by_games.sort { |a, b| a[0] <=> b[0] }.each do |score_detail|
+    (date, score) = score_detail
+    case score
+    when ScheduleBuilder::SCORE_FOR_EMPTY_GAME_TIME
+      puts "#{date} has previous empty games"
+      empty_game_times += 1
+    when ScheduleBuilder::SCORE_FOR_CRAPPY_GAME_TIME
+      puts "#{date} is 2nd 'crappy' game time for team"
+      crappy_game_times += 1
+    when ScheduleBuilder::SCORE_FOR_GAME_ON_NON_PREFERRED_DAY
+      puts "#{date} is on non-preferred day"
+      nonpreferred_game_time += 1
+    when ScheduleBuilder::SCORE_FOR_GAME_ON_REQUESTED_DAY_OFF
+      puts "#{date} is on requested day off"
+      games_on_requested_days_off += 1
+    end
+  end
+
+  puts "# of games after empty slot: #{empty_game_times}"
+  puts "# of games that are a 2nd first or last game for team: #{crappy_game_times}"
+  puts "# of games that are on a nonpreferred day for a team: #{nonpreferred_game_time}"
+  puts "# of games that are on a team's requested day off: #{games_on_requested_days_off}"
+end
+
+def save_schedule(schedule)
+  puts "Writing schedule"
+  File.open('schedule.csv', 'w') do |file|
+    schedule.games.sort { |a, b| a.date <=> b.date }.each do |game|
+      file.puts [game.date.strftime(PickleSpears::DATE_FORMAT), game.team_ids.map { |id| Team[id].name }.flatten(1), Team[game.team_ids[0]].division.name ].join ","
+    end
+  end
+end
+
+season = Season.where(name: 'Spring 2013').first
+
+MENS_LEAGUE_ID = 1
+COED_LEAGUE_ID = 2
+WOMENS_LEAGUE_ID = 3
+
+schedule = Schedule.new(season,
+      [
+        {
+          league_ids: [ MENS_LEAGUE_ID, WOMENS_LEAGUE_ID ],
+          slot_info: OpenStruct.new({ cwday: 1, num_games: 5, first_game_time: '18:10'}),
+        },
+        {
+          league_ids: [ MENS_LEAGUE_ID, WOMENS_LEAGUE_ID ],
+          slot_info: OpenStruct.new({ cwday: 2, num_games: 5, first_game_time: '18:10'}),
+        },
+        {
+          league_ids: [ MENS_LEAGUE_ID, WOMENS_LEAGUE_ID ],
+          slot_info: OpenStruct.new({ cwday: 3, num_games: 5, first_game_time: '18:10'}),
+        },
+        {
+          league_ids: [ MENS_LEAGUE_ID, WOMENS_LEAGUE_ID ],
+          slot_info: OpenStruct.new({ cwday: 4, num_games: 5, first_game_time: '18:10'}),
+        },
+        {
+          league_ids: [ COED_LEAGUE_ID ],
+          slot_info: OpenStruct.new({ cwday: 5, num_games: 8, first_game_time: '18:10'}),
+        },
+        {
+          league_ids: [ COED_LEAGUE_ID ],
+          slot_info: OpenStruct.new({ cwday: 6, num_games: 11, first_game_time: '13:10'}),
+        },
+        {
+          league_ids: [ MENS_LEAGUE_ID, COED_LEAGUE_ID, WOMENS_LEAGUE_ID ],
+          slot_info: OpenStruct.new({ cwday: 7, num_games: 12, first_game_time: '13:10'}), # sunday is flex day
+        },
+      ])
+
+CSV.read(ARGV[0]).each do |r|
+  (date_str, home_team, away_team) = r
+
+  date = DateTime.strptime(date_str, '%a %b %e %I:%M %p')
+
+  home = season.teams.select { |t| t.name == home_team }.first
+  away = season.teams.select { |t| t.name == away_team }.first
+
+  schedule.games << OpenStruct.new(
+    team_ids: [home.id, away.id],
+    league_id: home.division.league.id,
+    league_ids: home.division.league.id,
+    date: date.to_time)
+end
+
+score_schedule(ScheduleBuilder.new(schedule))
+save_schedule(schedule)
