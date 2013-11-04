86822d49f1ea666d8a5a3ebe3172eb4f8d93113d
Ben Prew <ben.prew@gmail.com>
2013-11-03 16:13:50 -0800

    Fix bug creating duplicate games when creating a new team

--- bin/update_schedule_from_files.rb
+++ bin/update_schedule_from_files.rb
@@ -5,66 +5,57 @@ require 'pp'
 require_relative '../picklespears'
 
 def deal_with_missing_team(name, division, game_date, game_description, is_home_team=false)
-  if @@force_team_create == true
+  if @force_team_create == true
     warn "Creating Team: #{name}"
-    game = Game.create(
-      :date => game_date,
-      :description => game_description,
-    )
-    TeamsGame.find_or_create(
-      game_id: game.id,
-      team_id: Team.create(:name => name, :division_id => division.id).id,
-      ) { |tg| tg.is_home_team = is_home_team}
+    Team.create(name: name, division_id: division.id)
   else
     missing_teams << "#{name} #{division.name}"
+    []
   end
 end
 
-def add_game_for_team(division, team, is_home_team=false, game_date, game_description)
-  teams = Team.filter(:name => team).all
-  if teams.length == 0
-    deal_with_missing_team(team, division, game_date, game_description, is_home_team)
-    return
+def add_game_for_team(division, team_name, is_home_team = false, game_date, game_description)
+  teams = Team.where(name: team_name).all.reject do |t|
+    t.division.league != division.league
   end
-  found_team = false
 
-  teams.each do |t|
-    next unless t.division.league == division.league
+  if teams.length == 0
+    teams << deal_with_missing_team(
+      team_name,
+      division,
+      game_date,
+      game_description,
+      is_home_team)
+  end
 
-    found_team = true
-    if t.division != division
-      warn "TEAM: #{team} - Updating division from #{t.division.name} to #{division.name}"
-      t.division = division
-      t.save
+  teams.each do |team|
+    if team.division != division
+      warn "TEAM: #{team.name} - Division: #{team.division.name} to #{division.name}"
+      team.division = division
+      team.save
     end
     game = Game.find_or_create(
-      :date => game_date,
-      :description => game_description
-      )
+      date: game_date,
+      description: game_description)
 
-    TeamsGame.find_or_create(
-      game_id: game.id,
-      team_id: t.id
-      ) { |tg| tg.is_home_team = is_home_team }
+    is_home_team ? game.home_team = team : game.away_team = team
   end
-
-  deal_with_missing_team(team, division, game_date, game_description, is_home_team) unless found_team
 end
 
 missing_teams = Set.new
-@@force_team_create = true
+@force_team_create = true
 
 ARGV.each do |filename|
   puts filename
   f = File.new(filename)
   f.each do |line|
     line.chop!
-    (league_name, division, home, away, game_date, game_description) = line.split "|"
+    (league_name, division, home, away, date, description) = line.split '|'
 
-    league = League.first(:name => league_name)
-    division = Division.first(:name => division, :league => league)
-    add_game_for_team(division, home, true, game_date, game_description)
-    add_game_for_team(division, away, false, game_date, game_description)
+    league = League.first(name: league_name)
+    division = Division.first(name: division, league: league)
+    add_game_for_team(division, home, true, date, description)
+    add_game_for_team(division, away, false, date, description)
   end
 end
 
