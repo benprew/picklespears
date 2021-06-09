2932574f488b4a1d0e41da2ad6e269a0f832d25d
Ben Prew <ben@throwingbones.com>
2021-06-08 22:47:06 -0700

    Fix docker compose to wait for database to come up

---
+++ wait-for-postgres.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+host=$1
+shift
+cmd="$@"
+
+until psql "$DATABASE_URL"; do
+      >&2 echo "Postgres is unavailable - sleeping"
+      sleep 2
+done
+
+>&2 echo "Postgres is up - executing command"
+exec "$cmd"
